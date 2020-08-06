54c89aff214d4f0f742b2d2ec783dd33e1d4adbc
Matthias Geier <Matthias.Geier@gmail.com>
2020-08-06 12:22:01 +0200

    Add ASDF favicon

--- doc/conf.py
+++ doc/conf.py
@@ -76,6 +76,7 @@ except Exception:
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'insipid'
+html_favicon = 'favicon.svg'
 html_add_permalinks = '\N{SECTION SIGN}'
 html_copy_source = False
 html_show_copyright = False
